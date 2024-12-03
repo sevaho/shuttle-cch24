b3c2f38ceb544a06869bdff149321926334cfc4c
sevaho <sebastiaan@sevaho.io>
2024-12-03 21:22:19 +0100

    Add first challenge

--- justfile
+++ justfile
@@ -1,3 +1,9 @@
+help:
+    just -l
+
 run:
     shuttle run --working-directory ./shuttlings-cch24
+
+deploy:
+    shuttle deploy --name shuttlings-cch24 --working-directory ./shuttlings-cch24
 
--- shuttlings-cch24/src/main.rs
+++ shuttlings-cch24/src/main.rs
@@ -1,12 +1,30 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::get,
+    Router,
+    response::Response,
+    http::{StatusCode, header::LOCATION},
+};
 
 async fn hello_world() -> &'static str {
-    "Hello, world!"
+    "Hello, bird!"
+}
+
+async fn seek_302() -> Response {
+    let redirect_url = "https://www.youtube.com/watch?v=9Gc4QTqslN4";
+
+    // Build the 302 response
+    Response::builder()
+        .status(StatusCode::FOUND)
+        .header(LOCATION, redirect_url)
+        .body(axum::body::Body::empty())
+        .unwrap()
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/-1/seek", get(seek_302))
+        .route("/", get(hello_world));
 
     Ok(router.into())
 }
